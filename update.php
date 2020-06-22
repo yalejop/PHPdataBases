048a998c7aa15106f005c3050659df9ec84e9ea7
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-06-22 13:17:25 -0500

    Actualización de usuarios en la base de datos

--- update.php
+++ update.php
@@ -1,3 +1,41 @@
+<?php 
+
+include_once 'config.php';
+$result = false;
+
+if (!empty($_POST)) {
+    $id = $_POST['id'];
+    $newName = $_POST['name'];
+    $newEmail = $_POST['email'];
+
+    $sql = "UPDATE users SET name=:name, email=:email WHERE id=:id";
+    $query = $pdo->prepare($sql);
+
+    $result = $query->execute([
+        'id' => $id,
+        'name' => $newName,
+        'email' => $newEmail
+    ]);
+
+    $nameValue = $newName;
+    $emailValue = $newEmail;
+}else {
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM users WHERE id=:id";
+    $query = $pdo->prepare($sql);
+
+    $query->execute([
+        'id' => $id
+    ]);
+
+    $row = $query->fetch(PDO::FETCH_ASSOC);
+    $nameValue = $row['name'];
+    $emailValue = $row['email'];
+}
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,14 +51,21 @@
 </head>
 <body>
     <div class="container">
-    <H1>Update Users</H1>
+    <h1>Update Users</h1>
+    <a href="list.php">Back</a>
+    <?php
+    if ($result) {
+                echo '<div class="alert alert-success">Sucess!!</div>';
+            }
+    ?>       
     <form action="update.php" method="post">
         <label for="name">Name:</label>
-        <input type="text" name="name" id="name">
+        <input type="text" name="name" id="name" value="<?php echo $nameValue; ?>">
         <br>
         <label for="email">Email:</label>
-        <input type="email" name="email" id="email">
+        <input type="email" name="email" id="email" value="<?php echo $emailValue; ?>">
         <br>
+        <input type="hidden" name="id" value="<?php echo $id ?>">
         <input type="submit" value="Update">
     </form>
     </div>
